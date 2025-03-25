1b72ce60e2b8bf7f4bda7e43c68bcc8f860f94ce
Jeancyno <plamedibakulu406@gmail.com>
2025-03-25 20:54:55 +0100

    [UPDATE]index

---
+++ pages/index.php
@@ -0,0 +1,9 @@
+<?php include 'includes/header.php'; ?>
+
+<h1 class="text-2xl font-bold mb-4">Classification d'un animal</h1>
+
+
+<form action="process.php" method="POST" class="bg-white p-6 shadow-md rounded">
+    la page ici........
+</form>
+<?php include 'includes/footer.php'; ?>
